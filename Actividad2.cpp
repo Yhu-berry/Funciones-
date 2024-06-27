9c37319ea9830d0e8fd6ef79ed5225c285cf7a54
Yhu-berry <ynmaqueraf@unjbg.edu.pe>
2024-06-26 21:06:51 -0500

    Actividad2: Implementando la funcion srand y time(0)

--- Actividad2.cpp
+++ Actividad2.cpp
@@ -2,22 +2,28 @@
 
 //inlusión de librerias
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
+#include <cstdlib> /*Para poder implementar rand() y srand()*/
+#include <ctime> 
 using namespace std;
 
 int main(){
     int n,numM;
-    //Solicitar datos de entrada
+    cout<<endl<<"SERIE DE NUMEROS ALEATORIOS \n";
+    //Solicitar datos de entradas
     cout<<"Cantidad de numeros aleatorios: ";cin>>n;
     cout<<"Numero maximo: ";cin>>numM;
 
-    //mostrar la serie de números aleatorios
+    //Generador de numeros aleatorios basada en el tiempo
+      srand(time(0));
+    /*Es como una semilla de minecraft te puede aparecer un mundo igual al otro 
+    ,pero mayormente se crean mundos aleatoriamente*/
+  
+    //Mostrar la serie de números aleatorios
     for(int i=0; i<n;i++){
         int numAleatorio= rand() % (numM+1); //% delimita al numAleatorio hasta el numM+1
-        cout<< numAleatorio<< " ";
+        cout<<numAleatorio<< " ";
     }
-        cout<<"\n";
+        cout<<"\n"<<" ";
 
     return 0;
 }
