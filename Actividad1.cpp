b78fd5010db8aab30d96ce65c7f497cb4fc9a2dd
Yhu-berry <ynmaqueraf@unjbg.edu.pe>
2024-06-26 20:58:50 -0500

    Actividad1: Agregada una mejora en la interfaz de usuario

--- Actividad1.cpp
+++ Actividad1.cpp
@@ -5,9 +5,15 @@ using namespace std;
 
 int main(){
     double A,r;
+
+    cout<<endl<<"CALCULAR El AREA DE UN CIRCULO \n";
     cout<<"Ingrese el radio: ";
     cin>>r;
+
     A=M_PI * pow(r,2);
-    cout<<A<<endl;
+
+    cout<<"A: "<<A;
+    cout<<"\n"<<" ";
+
     return 0;
 }
