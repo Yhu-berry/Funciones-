0ff0308234b075217ad920916a15050edd1c0f34
Yhu-berry <ynmaqueraf@unjbg.edu.pe>
2024-06-29 17:01:03 -0500

    Actividad4: Se agregó una función para delimitar los datos de entrada a solo numéricos

--- Actividad4.cpp
+++ Actividad4.cpp
@@ -2,30 +2,55 @@
 #include <iostream>
 #include <cmath>
 #include <complex> //incluye el conjunto de los numeros complejos 
+#include <limits> //Proporciona informacion sobre las carcteristicas del tipo numericas
 using namespace std;
+
+//Funcion para verificar si se ha ingresado un valor numérico
+void es_unNum();
 
 int main(){
     int a,b,c;
     //solicitar datos de entrada al usuario
     cout<<"Introduce los coeficientes"<<endl;
-    cin>>a>>b>>c;
+
+    cout<<"a: ";cin>>a;
+    es_unNum(); 
+
+    cout<<"b: ";cin>>b;
+    es_unNum(); 
+
+    cout<<"c: ";cin>>c;
+    es_unNum(); 
 
     double d=pow(b,2)-4*a*c;
 
-    //verificar si pertenece a los reales o no
-    if(d>0 || d==0){
+    //Verificar si pertenece a los reales o no
+    if(d>=0){
         double r1,r2;
         r1=(-b+sqrt(d))/(2*a);
         r2=(-b-sqrt(d))/(2*a);
         cout<<"r1: "<<r1<<endl;
         cout<<"r2: "<<r2<<endl;
-    }else{
-        //raices complejas
+    }else{  
+    //raices complejas
         complex<double> r1,r2;
         r1=complex<double>(-b/(2*a),sqrt(-d)/(2*a));
         r2=complex<double>(-b/(2*a),-sqrt(-d)/(2*a));
         cout<<"r1: "<<r1<<endl;
         cout<<"r2: "<<r2<<endl;
     }
+
     return 0;
+}
+
+void es_unNum(){
+    /*si falla*/
+    if(cin.fail()){
+        cout<<"Entrada invalida, se permiten solo numeros"<<endl;
+        cin.clear(); /*evita un bucle inecesario y limpia el flujo de entrada*/
+        cin.ignore(numeric_limits<streamsize>::max(),'\n');
+        
+        /*Detiene la ejecución cuando se halla ingresado un valor no numerico*/
+        exit(EXIT_FAILURE); 
+    }
 }
